e8d4134d6ac586eded9ced01768f491bf4b421cc
2302978588@qq.com <2302978588@qq.com>
2020-08-19 09:22:11 +0800

    通用模块

--- kiwi_rpc_framework/rpc-api/src/main/java/top/kiwi/rpc/api/HelloServiceImpl.java
+++ kiwi_rpc_framework/rpc-api/src/main/java/top/kiwi/rpc/api/HelloServiceImpl.java
@@ -19,6 +19,7 @@ public class HelloServiceImpl implements HelloService{
     public static void main(String[] args) {
         HelloServiceImpl hs = new HelloServiceImpl();
         String nihao = hs.hello(new HelloObject(10, "nihao"));
+        System.out.println(nihao);
     }
 
 }
